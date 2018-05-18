9441e4a4fc8d3db736d93f1f416dbcfffe11cbff
Felixvl <digitaleservice@gmail.com>
2018-05-18 14:02:22 +0200

    player name intro added

--- Demo.java
+++ Demo.java
@@ -1,6 +1,22 @@
 
 public class Demo {
     public static void main(String args[]){
-    	
+    	Team team1 = new Team();
+    	team1.teamName = "Feyenoord";
+    	System.out.println(team1.teamName);
+    	Team team2 = new Team();
+    	team2.teamName = "Ajax";
+    	team2.player.playerName = "Dirk Kuyt";
+    	System.out.println(team2.teamName + " has one player named: " + team2.player.playerName);
     }
+}
+class Team{
+	String teamName;
+	Player player = new Player();
+	
+	
+}
+
+class Player {
+	String playerName;
 }
