ceaef10ac79c4b986c6bd67d01308cae7784168d
Felixvl <digitaleservice@gmail.com>
2018-05-18 14:12:41 +0200

    voostellen is gelukt

--- Demo.java
+++ Demo.java
@@ -3,16 +3,23 @@ public class Demo {
     public static void main(String args[]){
     	Team team1 = new Team();
     	team1.teamName = "Feyenoord";
-    	System.out.println(team1.teamName);
+    	team1.player.playerName = "Johan Cruijf";
+    	team1.stelSpelersVoor();
+//    	System.out.println(team1.teamName + " has one player named: " + team1.player.playerName);
     	Team team2 = new Team();
     	team2.teamName = "Ajax";
     	team2.player.playerName = "Dirk Kuyt";
-    	System.out.println(team2.teamName + " has one player named: " + team2.player.playerName);
+    	team2.stelSpelersVoor();
+//    	System.out.println(team2.teamName + " has one player named: " + team2.player.playerName);
     }
 }
 class Team{
 	String teamName;
 	Player player = new Player();
+	
+	void stelSpelersVoor() {
+		System.out.println(this.teamName + " has one player called: " + this.player.playerName);
+	}
 	
 	
 }
