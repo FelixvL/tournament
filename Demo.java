e650b0edf4b8ff29d14415d3e2935d034cc3e726
Felixvl <digitaleservice@gmail.com>
2018-05-18 16:30:56 +0200

    wedstrijd is made

--- Demo.java
+++ Demo.java
@@ -6,19 +6,32 @@ public class Demo {
 		tournament.spelersmaken();
 		Team ajax = tournament.team1;
 		Team feyenoord = tournament.team2;	
-		Wedstrijd pete = new Wedstrijd(ajax, feyenoord);
+		Wedstrijd wedstrijd = new Wedstrijd(ajax, feyenoord); // today
+		wedstrijd.advertise();  // 1 month
+		wedstrijd.playing();
 	}
 	
 }
 
 class Wedstrijd{
-	Team thuisTeam = new Team(); 
-	Team uitTeam = new Team();
+	Team thuisTeam; 
+	Team uitTeam;
+	
+	void playing() {
+//		new Wedstrijd(thuisTeam, uitTeam)
+		System.out.println("Wedstrijd wordt gespeeld tussen "+thuisTeam.teamName+" en "+ uitTeam.teamName);
+		thuisTeam.stelSpelersVoor();
+		uitTeam.stelSpelersVoor();
+	}
 	
 	Wedstrijd(Team thuisTeam, Team uitTeam){
-		
-		System.out.println("Wedstrijd wordt gespeeld tussen "+thuisTeam.teamName+" en "+ uitTeam.teamName);
-		
+		System.out.println("It is decided that this game will be played in the future");
+//		
+		this.thuisTeam = thuisTeam;
+		this.uitTeam = uitTeam;
+	}
+	void advertise() {
+		System.out.println("Advertisement Buy your tickets");
 	}
 	
 }
